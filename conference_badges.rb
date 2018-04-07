a0b7f29fd2c96eb899ab2a619b88c62139307a04
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-07 22:48:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  newarray = []
+  array.each do |i|
+  newarray << "Hello, my name is #{name}."
+  end
+  return newarray
+end
